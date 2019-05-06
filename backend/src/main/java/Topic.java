177ec098940209e154dc4c87025eaf52feb1d210
Elitsa Marinovska <21998037+elit0451@users.noreply.github.com>
2019-05-06 08:36:02 +0200

    Implemented User stories - Add, Get and Delete Course (#26)

--- backend/src/main/java/Topic.java
+++ backend/src/main/java/Topic.java
@@ -1,12 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+
 public class Topic {
 
     private String _name;
+    private HashMap<String, Course> courses;
 
     public Topic(String name) {
         _name = name;
+        courses = new HashMap<String, Course>();
     }
 
     public String getName() {
         return _name;
+    }
+
+    public boolean addCourse(String name, Teacher teacher, String roomNr) {
+        if(courses.containsKey(name.toLowerCase()) || !teacher.isEligible())
+            return false;
+
+        Course newCourse = new Course(name, teacher, roomNr);
+        courses.put(name.toLowerCase(), newCourse);
+        // ex: TopicName.CourseName
+        teacher.addCourse(_name + "." + name);
+        return true;
+    }
+
+    public ArrayList<Course> getCourses() {
+        return new ArrayList<Course>(courses.values());
+    }
+
+    public Course getCourse(String courseName) {
+        if(courses.containsKey(courseName.toLowerCase()))
+            return courses.get(courseName.toLowerCase());
+
+        throw new NoSuchElementException();
+    }
+
+    public void deleteCourse(String courseName){
+        if(courses.containsKey(courseName.toLowerCase()))
+            courses.remove(courseName.toLowerCase());
+        else
+            throw new NoSuchElementException();
     }
 }
---
+++ backend/src/test/java/CourseTests.java
@@ -0,0 +1,107 @@
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+public class CourseTests {
+    Topic topic;
+
+    @BeforeEach
+        //Here the repository would be initialized before each test.
+    void topicSetup(){
+        topic = new Topic("Language");
+
+        Teacher mockedTeacher1 = mock(Teacher.class);
+        when(mockedTeacher1.isEligible()).thenReturn(true);
+        topic.addCourse("French", mockedTeacher1, "107");
+    }
+
+    //Test if we can successfully add a course
+    @Test
+    void shouldUpdateListOfCoursesPerTopicWhenAdding() {
+        // mock teacher creation
+        Teacher mockedTeacher = mock(Teacher.class);
+
+        when(mockedTeacher.isEligible()).thenReturn(true);
+
+        topic.addCourse("English", mockedTeacher, "203");
+
+        assertEquals(2, topic.getCourses().size());
+    }
+
+    @Test
+    void shouldNotAddCourseToNotEligibleTeacher() {
+        // mock teachers creation
+        Teacher mockedTeacher = mock(Teacher.class);
+
+        when(mockedTeacher.isEligible()).thenReturn(false);
+
+        boolean addedCourse = topic.addCourse("English", mockedTeacher, "203");
+
+        assertFalse(addedCourse);
+    }
+
+    @Test
+    void shouldNotAddCoursesWithSameNameDiffCapitalization(){
+        Teacher mockedTeacher = mock(Teacher.class);
+
+        when(mockedTeacher.isEligible()).thenReturn(true);
+
+        topic.addCourse("English", mockedTeacher, "203");
+        boolean addedCourse = topic.addCourse("eNGliSH", mockedTeacher, "203");
+
+        assertFalse(addedCourse);
+    }
+
+    //TODO Implement a test to verify the teacher's course count is incremented
+
+    @Test
+    void courseShouldBeReturnedIfCourseNameExists(){
+        Course french = topic.getCourse("French");
+
+        assertEquals("107", french.getRoomNr());
+    }
+
+    @Test
+    void courseNameShouldBeCaseInsensitive(){
+
+        Course french = topic.getCourse("FrEnCH");
+
+        assertEquals("107", french.getRoomNr());
+    }
+
+    @Test
+    void shouldThrowExceptionIfCourseDoesNotExist(){
+        assertThrows(NoSuchElementException.class, () -> {
+            topic.getCourse("Danish");
+        });
+    }
+
+    @Test
+    void shouldUpdateListOfCoursesPerTopicWhenDeleting(){
+        int currentCoursesSize = topic.getCourses().size();
+
+        topic.deleteCourse("French");
+
+        assertEquals(currentCoursesSize - 1 , topic.getCourses().size());
+    }
+
+    @Test
+    void shouldThrowExceptionIfCourseDoesNotExistWhenDeleting(){
+        assertThrows(NoSuchElementException.class, () -> {
+            topic.deleteCourse("Danish");
+        });
+    }
+
+    @Test
+    void courseNameShouldBeCaseInsensitiveWhenDeleting(){
+        int currentCoursesSize = topic.getCourses().size();
+
+        topic.deleteCourse("FreNCH");
+
+        assertEquals(currentCoursesSize - 1 , topic.getCourses().size());
+    }
+}
