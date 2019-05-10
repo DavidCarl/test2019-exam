701871e11521a46a6517afd4d373b572c10cc8f2
David <david.martin.carl@gmail.com>
2019-05-11 00:44:52 +0200

    Updated version

--- backend/src/test/java/ApiTest.java
+++ backend/src/test/java/ApiTest.java
@@ -1,6 +1,8 @@
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
@@ -11,12 +13,14 @@ import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ApiTest {
 
     @Test
-    void firstapitest() {
+    void helloworld_statuscode() {
         HttpUriRequest request = new HttpGet( "http://localhost:8080/2/api/helloworld" );
         HttpResponse httpResponse = null;
         try {
@@ -25,6 +29,36 @@ public class ApiTest {
             e.printStackTrace();
         }
 
-        System.out.println(httpResponse.getStatusLine().getStatusCode());
+        System.out.println();
+        assertEquals(httpResponse.getStatusLine().getStatusCode(), 200);
+    }
+
+    //This is not the optimal way to do it
+    //AFAIK there should be a better library for API request, just cant remember it
+    //Ill update ASAP!
+    @Test
+    void helloworld_content() {
+        HttpUriRequest request = new HttpGet( "http://localhost:8080/2/api/helloworld" );
+        HttpResponse httpResponse = null;
+        try {
+            httpResponse = HttpClientBuilder.create().build().execute( request );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        try {
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent()), 65728);
+            String line = null;
+
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        }
+        catch (IOException e) { e.printStackTrace(); }
+        catch (Exception e) { e.printStackTrace(); }
+
+        assertEquals(sb.toString(), "Hello World");
     }
 }
